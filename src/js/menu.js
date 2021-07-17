8c5fcbe14aa7c03ca6b3d03067eab2dc94fb64b9
htmlpluscss <79198889134@ya.ru>
2021-07-17 10:11:37 +0300

    меню

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,3 +2,7 @@
 
 	document.querySelector('.btn-menu-toggle').addEventListener('click', () => document.body.classList.toggle('menu-show'));
 
+
+	Array.from(document.querySelectorAll('.menu__link'), link =>
+		link.addEventListener('click', () =>
+			document.body.classList.remove('menu-show')));
