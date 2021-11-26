abbe7124f9c45079cc84f5429ba4fcb6a7dc7b9b
Shimis <mr.shimis@gmail.com>
2021-11-26 19:51:49 +0300

    add info block on top + update Mikhail info

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,20 @@
 // btn toggle
 
-	document.querySelector('.btn-menu-toggle').addEventListener('click', () => document.body.classList.toggle('menu-show'));
+	document.querySelector('.btn-menu-toggle').addEventListener('click', () => {
+		document.body.classList.toggle('menu-show');
+		var infoBlock = document.getElementById('top_info_block');
+		console.log("Style: ", window.getComputedStyle(infoBlock, null).getPropertyValue('display'))
+		if(window.getComputedStyle(infoBlock, null).getPropertyValue('display') === 'none'){
+			document.getElementById('top_info_block').style.display = 'block';
+			document.getElementById('header_block').style.paddingTop = '0px';
+		}else{
+			document.getElementById('top_info_block').style.display = 'none';
+			document.getElementById('header_block').style.paddingTop = '20px';
+		}
+	});
 
 
 	Array.from(document.querySelectorAll('.menu__link'), link =>
-		link.addEventListener('click', () =>
-			document.body.classList.remove('menu-show')));
+		link.addEventListener('click', () =>{
+			document.body.classList.remove('menu-show');
+		}));
